a6b42813435aec9341e6f38d3aaa43acba342fba
LHENI <113351904+LHENI@users.noreply.github.com>
2023-04-27 13:49:56 +0200

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -1,3 +1,12 @@
+/*
+* File: App.java
+* Author: Laczkó Henrietta
+* Copyright: 2023, Laczkó Henrietta
+* Group: Szoft I-I-N
+* Date: 2023-04-27
+* Github: https://github.com/LHENI/lelepi.git
+* Licenc: GNU GPL
+*/
 public class App {
     public static void main(String[] args) throws Exception {
         new Parallelepiped().startParallelepiped();
--- src/Parallelepiped.java
+++ src/Parallelepiped.java
@@ -1,20 +1,48 @@
+/*
+* File: Parallelepiped.java
+* Author: Laczkó Henrietta
+* Copyright: 2023, Laczkó Henrietta
+* Group: Szoft I-I-N
+* Date: 2023-04-27
+* Github: https://github.com/LHENI/lelepi.git
+* Licenc: GNU GPL
+*/
 import java.util.Scanner;
 
 public class Parallelepiped {
 
     public Parallelepiped() {}
     public void startParallelepiped() {
+        this.about();
         double result =  this.inputData();
-        nyomtat(result);
+        priiint(result);
     }
-    private void nyomtat(double result) {
+    public void about(){
+        System.out.println("Laczkó Henrietta");
+        System.out.println("Szoft I/1/N");
+        System.out.println("2023-04-27");
+    }
+    
+    private void priiint(double result) {
         System.out.println(result);
     }
     private double inputData() {
         String AsideStr = Input("A oldal: ");
+        if (!this.checkInput(AsideStr)) {
+            System.out.println("HIBAAA!Csak számot lehet beírni!");
+            throw new NumberFormatException("Csaaaak számot lehet írni!!");
+        }
         String BsideStr = Input("B oldal: ");
+        if (!this.checkInput(BsideStr)) {
+            System.out.println("HIBAAA!Csak számot lehet beírni!");
+            throw new NumberFormatException("Csaaaak számot lehet írni!!");
+        }
         String CsideStr = Input("C oldal: ");
-        
+        if (!this.checkInput(CsideStr)) {
+            System.out.println("HIBAAA!Csak számot lehet beírni!");
+            throw new NumberFormatException("Csaaaak számot lehet írni!!");
+        }
+
         double Aside = Double.parseDouble(AsideStr);
         double Bside = Double.parseDouble(BsideStr);
         double Cside = Double.parseDouble(CsideStr);
